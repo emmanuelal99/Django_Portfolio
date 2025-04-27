4acbb1d72346c8093e19a676373e98ff7ffc554d
emmanuelal99 <emmanuelal99700@gmail.com>
2025-04-26 20:04:18 -0400

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -164,6 +164,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# Set admin email directly
+ADMIN_EMAIL = 'emmanuelal99700@gmail.com'
 
 # =========================================
 # LOGGING CONFIGURATION
